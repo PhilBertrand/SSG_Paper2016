79ca98effe935c1573d9faa15de0e60007dfb515
Philip Bertrand <PhilBertrand@users.noreply.github.com>
2017-04-11 17:05:33 -0400

    Add files via upload

---
+++ sorc.R
@@ -0,0 +1,23 @@
+sorc <- function(x){
+
+  if (!inherits(x, "list")) 
+    stop(paste("This function requires a list. You passed a", class(x)))
+  
+C <- rep(NA, length(x))
+Ci <- rep(NA, length(x)-1)
+val <- NULL
+
+for(j in 1:length(x[[1]])){
+  X <- sapply(x, "[", j)
+  for (i in 2:length(X)){
+    C[i] <- X[i] - X[i - 1]
+  }
+  for (i in 2:length(X)-1){
+    Ci[i] <- abs(C[i + 1] - C[i])
+  }
+  val[[j]] <- c(Ci)
+ }
+
+return(val)
+
+}
